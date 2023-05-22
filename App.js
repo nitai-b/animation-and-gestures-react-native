df9595aa7b0efef7aad450baa8e495d9ddaa7f70
nitai <nitai.barran@enquiretechit.com>
2023-05-22 14:20:25 -0400

    timing animation example with circle

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useRef} from 'react';
+import {Animated, Button, StyleSheet, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const leftValue = useRef(new Animated.Value(0)).current;
+
+    const moveBall = () => {
+        Animated.timing(leftValue, {
+            toValue: 1000,
+            duration: 1000,
+            useNativeDriver: false
+        }).start()
+    }
+
+    return (
+        <View style={styles.container}>
+            <Animated.View style={[{
+                width: 100,
+                height: 100,
+                marginLeft: leftValue,
+                borderRadius: 50,
+                backgroundColor: 'red',
+            }]}>
+
+            </Animated.View>
+            <Button onPress={moveBall} title="Click me" style={{marginTop: 'auto'}}/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
